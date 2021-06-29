a9ed3a9302e4a521c59ce0e4323d9914e2790c05
YYnfante <nobu027.99@gmail.com>
2021-06-29 17:43:05 -0400

    +Continue at home /

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 
-const Sidebar = ({name}) => (
-    <div className="wrapper">
-        <nav id="sidebar">
-            <div className="sidebar-header">
-                <h3>{name}</h3>
-            </div>
-            <ul className="list-unstyled components">
-                <p>Dummy Heading</p>
-                <li >
-                    <a href="#HomeSubMenu" aria-expanded="false" className="dropdown-toggle">Home</a>
-                </li>
+const Sidebar = ({Logo, name}) => (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-bg-dark">
+        <a className="navbar-brand" href="#/">{name}</a>
 
-
-
-
-            </ul>
-        </nav>
-    </div>
+    </nav>
 );
 
 export { Sidebar };
