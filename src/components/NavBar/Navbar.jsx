1997e75541d48e8600c862c85825e8d6517741e5
YYnfante <nobu027.99@gmail.com>
2021-07-01 17:54:33 -0400

    <------- Unuseful ---------->

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -15,17 +15,16 @@ const Navbar = ({name = "Translation", Logo, options = [], where}) => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand mb-0 h1" to={where} style={{paddingLeft:10}}>{name}</Link>
 
-        <div className={`collapse navbar-collapse ${mobileUser ? "show" : ""}`} id="navsc">
+        <div className={`collapse navbar-collapse ${mobileUser ? "show" : ""} navbar-fluid`} id="navsc">
             <ul className="navbar-nav mr-auto">
                 <NavBarOptions renderingOpts={options}/>
             </ul>
         </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
-        aria-label="Toggle navigation" onClick={() => setMobilUser(!mobileUser)}>
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="form-inline my-2 my-lg-0">
-
+        <div>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
+            aria-label="Toggle navigation" onClick={() => setMobilUser(!mobileUser)}>
+                <span className="navbar-toggler-icon"></span>
+            </button>
         </div>
     </nav>
 )};
